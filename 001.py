09973cd8d8c39a684d710bbc34ef42d97a326497
Petra Linhartova <matejickova.p@seznam.cz>
2025-04-28 22:14:06 +0200

    zakladni funkce - pridavani/mazani a vypsani tasku

--- 001.py
+++ 001.py
@@ -1,2 +1,39 @@
-pokus1
-pokus2
+def add_task(task):
+    todolist.append(task)
+    return todolist
+
+def remove_task(task):
+    todolist.remove(task)
+    return todolist
+
+def show_list(todolist):
+    print(todolist)
+    
+todolist = []
+
+while True:
+    v1 = int(input("What you want to do? Write 1 to add task, 2 to delete task, 3 to print the list or 4 to exit:\n"))
+    if v1 == 1:
+        task = input("Write task to add...\n")
+        if task in todolist:
+            print("This task is in To Do List already.")
+        else:
+            add_task(task)
+    elif v1 == 2:
+        task = input("Write task to delete...\n")
+        if task not in todolist:
+            print("This task is not in To Do List.")
+        else:
+            remove_task(task)
+    elif v1 == 3:
+        show_list(todolist)
+    elif v1 == 4:
+        break
+    else:
+        print("Enter a valid number")
+
+
+
+
+
+
