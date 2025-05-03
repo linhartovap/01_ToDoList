01695af8913a47823d421cf6404c3ee696fae362
Petra Linhartova <matejickova.p@seznam.cz>
2025-05-03 12:00:34 +0200

    vylepšen seznam úkolů - slovník s názvem/datem/trváním

--- 001.py
+++ 001.py
@@ -7,20 +7,36 @@ def remove_task(task):
     return todolist
 
 def show_list(todolist):
-    print(todolist)
+    if not todolist:
+        print("Your ToDoList is empty, do what you want!")
+    else:
+        print(f"In you ToDoList are following tasks:")
+        for n, task in enumerate(todolist, 1):
+            print(f"{n} : {task["name"]}; {task["date"]}; {task["duration"]}")
+    
     
 todolist = []
 
 while True:
     v1 = int(input("What you want to do? Write 1 to add task, 2 to delete task, 3 to print the list or 4 to exit:\n"))
     if v1 == 1:
-        task = input("Write task to add...\n")
+        task_name = input("Write task name to add...\n")
+        task_date = input("Write due date of this task...\n")
+        task_duration = input("Write task duration...\n")
+        task = {"name" : task_name, "date" : task_date, "duration" : task_duration}
+
         if task in todolist:
             print("This task is in To Do List already.")
         else:
             add_task(task)
+
+
+
     elif v1 == 2:
-        task = input("Write task to delete...\n")
+        task_name = input("Write task name to delete...\n")
+        task_date = input("Write due date of this task...\n")
+        task_duration = input("Write task duration...\n")
+        task = {"name" : task_name, "date" : task_date, "duration" : task_duration}
         if task not in todolist:
             print("This task is not in To Do List.")
         else:
